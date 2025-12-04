0082279579061fea9078c79bfd81d8bb925c1302
DevSantiagoO <147413420+DevSantiagoO@users.noreply.github.com>
2025-12-04 19:51:21 -0300

    Agrego comando SELECT, y el archivo PRIMEROS-PASOS

---
+++ clase03/Comandos/select.sql
@@ -0,0 +1,4 @@
+/* Para consultar todos los datos de una tabla debo hacer lo siguiente */
+SELECT * FROM users;
+/*Con la palabra reservada FROM indico de que tabla quiero consultar. En caso de querer consultar columnas o atributos especificos debemos separar dichas columnas por comas por ejemplo: */
+SELECT user, username FROM users;
---
+++ clase03/primeros-pasos.md
@@ -0,0 +1,9 @@
+# Creacion de tabla.
+**Primera tabla: USERS**
+
+![alt text](image.png)
+
+### Ingreso de datos
+
+recordar que el tipo de dato DATE guarda año, mes y día.
+Por convencion las palabras reservadas de SQL se colocan en mayúsculas por ejemplo SELECT.
